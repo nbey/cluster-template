30e44d0bb0721f457463bcc8b1ad2074dfed9057
Nafis Bey <984849+nbey@users.noreply.github.com>
2023-01-04 14:40:56 -0500

    fix: ssh key file permission

--- restore.sh
+++ restore.sh
@@ -103,6 +103,7 @@ mkdir -p "$complete_download_path"
 # Setup SSH Connection Credentials
 touch ~/db-ssh-key
 echo $SSH_KEY_ENCODED | base64 -d > ~/db-ssh-key
+chmod 600 ~/db-ssh-key
 
 # Test the SSH Connection
 ssh -q -i ~/db-ssh-key $SSH_USERNAME@$DB_HOST exit
